435fcec13518d984763a95a03020b1fb67d1b69d
ravina <vermaravina029@gmail.com>
2024-03-13 11:36:09 +0200

    model trainer added

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from CustomerChurnPrediction import logger
 
 from CustomerChurnPrediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CustomerChurnPrediction.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-#from CustomerChurnPrediction.pipeline.stage_03_data_transformation import  DataTransformationTrainingPipeline
-#from CustomerChurnPrediction.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from CustomerChurnPrediction.pipeline.stage_03_data_transformation import  DataTransformationTrainingPipeline
+from CustomerChurnPrediction.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from CustomerChurnPrediction.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
@@ -30,3 +30,25 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME='Data Transformation stage'
+try:
+        logger.info(f">>>>>>> stage{STAGE_NAME} started <<<<<<<<")
+        obj=DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n x=============x")
+except Exception as e:
+        logger.exception(e)
+        raise e    
+
+
+
+STAGE_NAME='Data Transformation stage'
+try:
+        logger.info(f">>>>>>> stage{STAGE_NAME} started <<<<<<<<")
+        obj=ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n x=============x")
+except Exception as e:
+         logger.exception(e)
+         raise e       
